f7364dd7dc5ed6f91d3cddd7af985a012eb4e9e6
Gurtejhundal <gurtejbir.29107@gmail.com>
2025-12-07 23:13:35 +0530

    Update Logo to TRAQO

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,6 +17,7 @@ export default function Navbar() {
         <nav className="navbar-custom">
             {/* Left: User Profile */}
             <div className="nav-left">
+                <img src="/logo.jpg" alt="TRAQO Logo" className="navbar-logo" style={{ height: '32px', marginRight: '1rem', borderRadius: '50%' }} />
                 <div className="profile-wrapper">
                     <UserButton afterSignOutUrl="/login" />
                     <span className="profile-label">Profile</span>
