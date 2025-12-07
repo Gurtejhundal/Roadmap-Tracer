058dc7d7137bd0af4c71d74534057ecc8b7a9b7e
Gurtejhundal <gurtejbir.29107@gmail.com>
2025-12-07 19:53:35 +0530

    Fix main.jsx syntax

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<ClerkProvider publishableKey={"pk_test_Y3VycmVudC1maXJlZmx5LTkyLmNsZXJrLmFjY291bnRzLmRldiQ"} afterSignOutUrl="/">
-    <App />
-</ClerkProvider>
-    </StrictMode >,
+import App from './App.jsx'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = "pk_test_Y3VycmVudC1maXJlZmx5LTkyLmNsZXJrLmFjY291bnRzLmRldiQ"
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key")
+}
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+            <App />
+        </ClerkProvider>
+    </StrictMode>,
 )
